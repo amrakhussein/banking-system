6703b71bf6e57738fd37c0f407f47e13e96c343e
Amro Abdelkamel <amromoorie@gmail.com>
2022-05-31 23:48:34 +0200

    Feat(Server): create user ctrl

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,4 +1,5 @@
-import { User } from '../models/Schemas'
+import mongoose from 'mongoose'
+import { User } from '../models/Schemas.js'
 
 export const fetchUsers = async (req, res) => {
   try {
@@ -14,6 +15,44 @@ export const fetchUsers = async (req, res) => {
     const userErrorJson = {
       success: false,
       message: "Error has occured while fetching users' data",
+    }
+    return res.status(500).json(userErrorJson)
+  }
+}
+
+export const createUser = async (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'It cannot be empty!',
+    })
+    return
+  }
+  const { username, email, accountBalance } = req.body
+  try {
+    const newUser = {
+      _id: new mongoose.Types.ObjectId,
+      username,
+      email,
+      accountBalance,
+      // transactions,
+    }
+    // const newUserCreated = await User.create(newUser)
+    const newUserCreated = await new User(newUser)
+    const result = await newUserCreated.save()
+    console.log('newUserCreated: ', result)
+
+    const usersInfoJson = {
+      success: true,
+      data: newUserCreated,
+    }
+
+    res.status(200).json(usersInfoJson)
+    mongoose.disconnect()
+
+  } catch (err) {
+    const userErrorJson = {
+      success: false,
+      message: "Error has occured while creating a user",
     }
     return res.status(500).json(userErrorJson)
   }
