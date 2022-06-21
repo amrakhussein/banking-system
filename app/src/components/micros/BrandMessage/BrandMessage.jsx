c7e2251f40fdcce86ab70cd65270b5923017df6e
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-21 14:23:26 +0200

    Feat(Frontend): add Home page

---
+++ app/src/components/micros/BrandMessage/BrandMessage.jsx
@@ -0,0 +1,13 @@
+import './BrandMessage.css'
+
+export default function BrandMessage() {
+  return (
+    <>
+      <p>Fakify Bank let you transfer money securely with ease.</p>
+      <p className='msg-important'>
+        {' '}
+        Start your first <strong>transaction!</strong>
+      </p>
+    </>
+  )
+}
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,8 +1,47 @@
+import { Button, Container, Form } from 'react-bootstrap'
+import BrandMessage from '../components/micros/BrandMessage/BrandMessage'
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
+    <Container className='mt-5'>
+      <section className='pb-5 h5 text-muted text-uppfercase jumbotron jumbotron-fluid'>
+        <div class='container display-6 '>
+          <BrandMessage />
+        </div>
+      </section>
+
+      <div className='d-flex flex-column justify-cofntent-center align-items-md-center'>
+        <section className='pt-5 pb-3 text-secondary'>
+          {/* <div class="container"> */}
+          <h3>Create a new user with balance amount</h3>
+          {/* <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> */}
+          {/* </div> */}
+        </section>
+
+        <Form>
+          <Form.Group className='mb-3 shadow-sm' controlId='formBasicEmail'>
+            <Form.Label>Username</Form.Label>
+            <Form.Control type='username' placeholder='Enter your username' />
+          </Form.Group>
+
+          <Form.Group className='mb-3 shadow-sm' controlId='formBasicPassword'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email' placeholder='Enter your Email' />
+          </Form.Group>
+
+          <Form.Group className='mb-3 shadow-sm ' controlId='formBasicPassword'>
+            <Form.Label>Account Balance</Form.Label>
+            <Form.Control
+              type='accountBalance'
+              placeholder='Enter your Account Balance'
+            />
+          </Form.Group>
+
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Container>
   )
 }
-
-export default Home
