161c249c70092d0a7722546c1fa57664d51a5689
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-21 06:39:56 +0200

    Feat(Server): connect database config

--- server/connectDatabase.js
+++ server/connectDatabase.js
@@ -1,27 +1,20 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import mongoose from 'mongoose'
 
 // import { DB_CONNECTION_URI } from './config'
-const { DB_USERNAME, DB_PASSWORD } = process.env
 
-const connectDatabase = async () => {
+const {DB_USERNAME, DB_PASSWORD} = process.env
+const MongoURI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.x6bbr.mongodb.net/?retryWrites=true&w=majority`
+
+const connectDatabase = () => {
   const config = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
   }
-  const client = new MongoClient(
-    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.x6bbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    config
-  )
-  try {
-    await client.connect()
-    console.log('connected to DADABASE')
-    await listDb(client)
-  } catch (e) {
-    console.error(e)
-  } finally {
-    await client.close()
-  }
+
+  mongoose.connect(MongoURI, config).then(console.log('connected to DADABASE'))
+  
 }
 
 export default connectDatabase
